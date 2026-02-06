965f1e8bda1973898ab8d2bd859b9a196848e56f
Prince <prince.chidera.dike@gmail.com>
2026-02-06 23:06:33 +0100

    ajsjadjk

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
     GOOGLE_SCRIPT_URL_LEAD_MAGNET: 'https://script.google.com/macros/s/AKfycbzIegCqon2wsF_nB8By_2LhrcdioxXs99Y8zj19z6QSnc7TynRg521b5aZjT7i3i5Ev/exec',
-    GOOGLE_SCRIPT_URL_APP: 'https://script.google.com/macros/s/AKfycbzIegCqon2wsF_nB8By_2LhrcdioxXs99Y8zj19z6QSnc7TynRg521b5aZjT7i3i5Ev/exec'
+    GOOGLE_SCRIPT_URL_APP: 'https://script.google.com/macros/s/AKfycbzIegCqon2wsF_nB8By_2LhrcdioxXs99Y8zj19z6QSnc7TynRg521b5aZjT7i3i5Ev/exec',
+    GOOGLE_SCRIPT_DASHBOARD_URL: 'https://script.google.com/macros/s/AKfycbzD2GdGF3GhyX2jVGvcXMdJyeVBGULOYKMPpEeF51HZ5EqjrIQ-wHreYvCjlKwPZD8vzQ/exec'
 };
